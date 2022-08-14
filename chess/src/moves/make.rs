74de97d28c32e5de0338e0ff3065f04fc464ebed
Alexander Kernozhitsky <sh200105@mail.ru>
2022-08-15 00:25:27 +0300

    Add docs

--- chess/src/moves/make.rs
+++ chess/src/moves/make.rs
@@ -1,5 +1,7 @@
-// TODO doc
-// TODO tests
+//! Types to apply the moves and move-like objects
+//!
+//! This module contains a trait `[Make]` and its implementations. Each implementation of this
+//! trait can be applied as a move.
 
 use super::base::{self, Move, RawUndo, ValidateError};
 use super::{san, uci};
@@ -7,11 +9,35 @@ use crate::board::Board;
 
 use core::convert::Infallible;
 
-pub trait Make {
+/// Trait for move-like objects
+///
+/// Each implementation of this trait can be applied as a chess move.
+///
+/// # Safety
+///
+/// The implementation of [`Make::make_raw()`] must return a valid pair of [`Move`] and [`RawUndo`],
+/// which can be safety passed to [`moves::unmake_move_unchecked`](base::unmake_move_unchecked).
+/// Also, the implementations of [`Make::make_raw()`] and [`Make::make()`] must be consistent with
+/// respect to each other.
+pub unsafe trait Make {
     type Err;
 
+    /// Applies the move to the board `board`
+    ///
+    /// In case of success, a tuple is returned. It contains an applied move and a [`RawUndo`],
+    /// which can be passed to [`moves::unmake_move_unchecked`](base::unmake_move_unchecked) to
+    /// revert this move. In case of error, the board is unchanged.
+    ///
+    /// If the move is not legal, then an error is returned. Some implementations (like [`Unchecked`]
+    /// or [`TryUnchecked`]) also allow making null moves.
     fn make_raw(&self, board: &mut Board) -> Result<(Move, RawUndo), Self::Err>;
 
+    /// Applied the move to the board `board`
+    ///
+    /// Returns the new board after applying the given move.
+    ///
+    /// If the move is not legal, then an error is returned. Some implementations (like [`Unchecked`]
+    /// or [`TryUnchecked`]) also allow making null moves.
     fn make(&self, board: &Board) -> Result<Board, Self::Err> {
         let mut cloned = board.clone();
         let _ = self.make_raw(&mut cloned)?;
@@ -19,16 +45,28 @@ pub trait Make {
     }
 }
 
+/// Wrapper to make a move without validity checks
+///
+/// # Safety
+///
+/// See the doc for [`Unchecked::new()`].
 pub struct Unchecked(Move);
 
 impl Unchecked {
+    /// Wraps a move `mv` into [`Unchecked`]
+    ///
+    /// # Safety
+    ///
+    /// You must guarantee that this move will be applied only to the position when this move is legal
+    /// or null, and null moves are allowed only if the king is not is check. Applying this move to other
+    /// positions is considered undefined behaviour.
     #[inline]
     pub unsafe fn new(mv: Move) -> Self {
         Self(mv)
     }
 }
 
-impl Make for Unchecked {
+unsafe impl Make for Unchecked {
     type Err = Infallible;
 
     #[inline]
@@ -38,16 +76,31 @@ impl Make for Unchecked {
     }
 }
 
+/// Wrapper a semilegal or null move that bypasses semi-legality checks
+///
+/// This wrapper makes a semilegal move and checks whether this move is legal after making it. No
+/// other validity checks are performed. So, it will return an error is the move is semilegal but not
+/// legal. If the move is null, an error is returned if and only if the king is in check.
+///
+/// # Safety
+///
+/// See the doc for [`TryUnchecked::new()`].
 pub struct TryUnchecked(Move);
 
 impl TryUnchecked {
+    /// Wraps a move `mv` into [`Unchecked`]
+    ///
+    /// # Safety
+    ///
+    /// You must guarantee that this move will be applied only to the position when this move is legal
+    /// or null, otherwise the behavior is undefined.
     #[inline]
     pub unsafe fn new(mv: Move) -> Self {
         Self(mv)
     }
 }
 
-impl Make for TryUnchecked {
+unsafe impl Make for TryUnchecked {
     type Err = ValidateError;
 
     #[inline]
@@ -71,7 +124,7 @@ impl Make for TryUnchecked {
     }
 }
 
-impl Make for Move {
+unsafe impl Make for Move {
     type Err = ValidateError;
 
     #[inline]
@@ -87,9 +140,10 @@ impl Make for Move {
     }
 }
 
+/// Wrapper to apply a move directly from its UCI notation
 pub struct Uci<S: AsRef<str>>(pub S);
 
-impl<S: AsRef<str>> Make for Uci<S> {
+unsafe impl<S: AsRef<str>> Make for Uci<S> {
     type Err = uci::ParseError;
 
     #[inline]
@@ -109,9 +163,10 @@ impl<S: AsRef<str>> Make for Uci<S> {
     }
 }
 
+/// Wrapper to apply a move directly from its SAN notation
 pub struct San<S: AsRef<str>>(pub S);
 
-impl<S: AsRef<str>> Make for San<S> {
+unsafe impl<S: AsRef<str>> Make for San<S> {
     type Err = san::ParseError;
 
     #[inline]
